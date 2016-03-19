7d9bccaa05b33d50596b82623251544ad7ca87ef
Peragorn <damian16172@gmail.com>
2016-03-19 13:15:05 +0100

    dodanie podstawowych przyciskow

--- app/src/main/java/com/firstproject/siaum/calculator/MainActivity.java
+++ app/src/main/java/com/firstproject/siaum/calculator/MainActivity.java
@@ -1,7 +1,7 @@
 package com.firstproject.siaum.calculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         if(bar != null){
             bar.hide();
         }
+
     }
 }
